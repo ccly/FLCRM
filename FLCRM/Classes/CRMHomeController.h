2d478995ec7c45bbf085bc6b63f554ebd78627fd
枫 <1060108978@qq.com>
2023-10-14 23:57:32 +0800

    删除

---
+++ FLCRM/Classes/CRMHomeController.h
@@ -0,0 +1,18 @@
+//
+//  CRMHomeController.h
+//  FLCRM
+//
+//  Created by 枫 on 2023/10/14.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CRMHomeController : UIViewController
+
+@property(nonatomic, strong) NSString *userId;
+
+@end
+
+NS_ASSUME_NONNULL_END
